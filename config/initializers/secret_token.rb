25c0331e4ec676d9e74a2d01db5f9434f765b777
Laura <laura@laura.laura.com>
2011-12-16 11:06:13 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hunter::Application.config.secret_token = '079e05a28f1c1e76e1dd776ef493cd4f064ef6977c1426d6f80b40ff13a5f38a9e54b25e3a44d6edf8f1b59996f38b025e26e6cc96c1307ca757fc8269e4fc1a'
