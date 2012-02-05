a65da71c68be62b3277c11fe7d3d3aa6fa70036c
Laura <laura.alisson86@gmail.com>
2012-02-05 14:15:48 -0800

    Completed contact form.

--- spec/mailers/request_mailer_spec.rb
+++ spec/mailers/request_mailer_spec.rb
@@ -4,13 +4,18 @@ describe RequestMailer do
 		ActionMailer::Base.deliveries = []
 	end 
 
-	it "should send request email " do 
-		RequestMailer.request_email("petitioner", "replyEmailAddress", "imageNumber", "description")
-		sent.first.subject.should = "Photo use request from throughhunterseyes.com"
-		sent.first.body.should =~ /Name: #{"petitioner"}/
-		sent.first.body.should =~ /Email address: #{"replyEmailAddress"}/
-		sent.first.body.should =~ /Image Number: #{"imageNumber"}/
-		sent.first.body.should =~ /Description: #{"description"}/
+	it "should send request email " do
+		petitioner = "petitioner"
+		replyEmailAddress = "replyEmailAddress"
+		imageNumber = "imageNumber"
+		description = "description"
+ 
+		RequestMailer.request_email(petitioner, replyEmailAddress, imageNumber, description).deliver
+		sent.first.subject.should =~ /Photo use request from throughhunterseyes.com/
+		sent.first.body.should =~ /Name: #{ petitioner }/
+		sent.first.body.should =~ /Email address: #{ replyEmailAddress }/
+		sent.first.body.should =~ /Image number: #{ imageNumber }/
+		sent.first.body.should =~ /Description of why #{ petitioner } would like to use the photo: #{ description }/
 	end 
 
 	def sent 
