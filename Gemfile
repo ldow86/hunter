bd87ae29d84f84ff4b72400aba9346925bbb1547
Laura <laura.alisson86@gmail.com>
2012-02-07 11:10:10 -0800

    Made changes to limit image size.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.11'
 gem 'pg'
+gem 'rmagick'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
+require 'RMagick'
+
 class Photo < ActiveRecord::Base
 
 	validates :binary_data, :presence => true
@@ -6,6 +8,9 @@ class Photo < ActiveRecord::Base
 	def image_file=(input_data)
 		self.filename = input_data.original_filename
 		self.content_type = input_data.content_type.chomp
-		self.binary_data = input_data.read 
+
+		image = Magick::Image.from_blob(input_data.read).first
+		smallimage = image.resize_to_fit(600, 600)
+		self.binary_data = smallimage.to_blob
 	end 
 end
